e32191748dbc703233d85ee01e7fdbe5672136da
Klove Adams <kloveadams@gmail.com>
2021-10-04 17:31:03 -0400

    wierd up accountsRouter starting to work on model

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,45 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.json("/api/accounts")
+  try {
+
+  }catch (err) {
+    next()
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+
+  }catch (err) {
+    next()
+  }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+
+  }catch (err) {
+    next()
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+  try {
+
+  } catch (err) {
+    next()
+  }
+}
+);
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+
+  }catch (err) {
+    next()
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
