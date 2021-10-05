aa454e76f6a4f300471a0a17a031281cf18628fc
Klove Adams <kloveadams@gmail.com>
2021-10-04 21:18:58 -0400

    update tests 15-21 are passing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -39,11 +39,11 @@ router.post(
 router.put(
   "/:id",
   checkAccountId,
-  checkAccountNameUnique,
   checkAccountPayload,
-  (req, res, next) => {
+  async (req, res, next) => {
     try {
-      res.json("put account");
+      const updated = await Account.updateById(req.params.id, req.body);
+      res.status(200).json(updated);
     } catch (err) {
       next(err);
     }
